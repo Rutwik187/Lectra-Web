64bf2d4a7c0b5f82fe83acbaa235872d879cb1c3
Rutwik187 <rutwikshinde34@gmail.com>
2023-01-04 00:31:59 +0530

    left side of hero created

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Logo from "../../assets/logo.png";
+import "./header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src={Logo} alt="company logo" />
+      </div>
+      <div className="header-menu">
+        <ul>
+          <li>Home</li>
+          <li>Mechanisms</li>
+          <li>Why Us</li>
+          <li>Plans</li>
+          <li>Our Team</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,11 +4,56 @@ import "./hero.css";
 
 const Hero = () => {
   return (
-    <div className="Hero">
-      <div className="Hero-left">
+    <div className="hero">
+      <div className="hero-left">
         <Header />
+        <div className="hero-tag_line">
+          <div className="hero-tag_line-div"></div>
+          <span className="hero-tag_line-span">
+            the best fitness friend in the town{" "}
+          </span>
+        </div>
+        <div className="hero-heading">
+          <div>
+            <span>Helping </span>
+            <span
+              className="
+            stroke-text"
+            >
+              You
+            </span>
+          </div>
+          <div>
+            <span>Recover </span>
+            <span className="stroke-text">Smarter</span>
+          </div>
+          <span className="hero-sub_heading">
+            Lectra Tape is a wireless and Portable Electric Muscle Stimulator
+            embedded into Athletic tape utilizing proprietary technology to
+            conduct the shock.
+          </span>
+        </div>
+        <div className="hero-figures">
+          <div>
+            <span>140+</span>
+            <span>stores</span>
+          </div>
+          <div>
+            <span>+1000</span>
+            <span>Pre-orders</span>
+          </div>
+          <div>
+            <span>50+</span>
+            <span>products</span>
+          </div>
+        </div>
+        <div className="hero-buttons">
+          <button className="btn ">Buy Now</button>
+          <button className="btn">Learn More</button>
+        </div>
       </div>
-      <div className="Hero-right">Right</div>
+
+      <div className="hero-right">right</div>
     </div>
   );
 };
