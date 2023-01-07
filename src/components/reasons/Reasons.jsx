5420b28b2d105637991486d7d751ac5c7c454dc0
Rutwik187 <rutwikshinde34@gmail.com>
2023-01-07 22:59:10 +0530

    reasons to join

---
+++ src/components/reasons/Reasons.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./reasons.css";
+import tick from "../../assets/tick.png";
+import adidas from "../../assets/adidas.png";
+import nike from "../../assets/nike.png";
+import video from "../../assets/video.png";
+
+const Reasons = () => {
+  return (
+    <div className="Reasons" id="reasons">
+      <div className="Reasons-left">
+        <img src={video} alt="video" />
+      </div>
+      <div className="Reasons-right">
+        <span>some reasons</span>
+        <div>
+          <span className="stroke-text">Why </span>
+          <span>Choose Us?</span>
+        </div>
+        <div className="Reasons-details-right">
+          <div>
+            <img src={tick} alt="tick"></img>
+            <span>Reason 1</span>
+          </div>
+          <div>
+            <img src={tick} alt="tick" />
+            <span>Reason 2</span>
+          </div>
+          <div>
+            <img src={tick} alt="tick" />
+            <span>Reason 3</span>
+          </div>
+          <div>
+            <img src={tick} alt="tick" />
+            <span>Reason 4</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Reasons;
